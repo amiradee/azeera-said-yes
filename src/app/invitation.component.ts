c5ea7bc80190d94bc89ca7cf39b5633efb831eed
Amira Deana <adbintiabraham1@sheffield.ac.uk>
2025-11-10 17:56:49 +0000

    add no of pax and audio

--- src/app/invitation.component.ts
+++ src/app/invitation.component.ts
@@ -1,32 +1,56 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   standalone: true,
-  templateUrl: './invitation.component.html',
-  styleUrl: './invitation.component.css',
-  imports: [ 
-    CommonModule,
-    FormsModule 
-  ],
+  templateUrl: "./invitation.component.html",
+  styleUrl: "./invitation.component.css",
+  imports: [CommonModule, FormsModule],
 })
 export class InvitationComponent {
   countdown: string | undefined;
   rsvp = {
-    name: '',
-    attendance: '',
-    message: ''
+    name: "",
+    attendance: "",
+    pax: "",
+    message: "",
   };
   isSubmitting = false;
   showSuccess = false;
-  lastSubmittedName = '';
+  lastSubmittedName = "";
 
   showCopySuccess = false;
+  isPlaying = true;
 
   ngOnInit() {
     this.setupCountdown();
+  }
+
+  ngAfterViewInit() {
+    const audio = document.getElementById("weddingSong") as HTMLAudioElement;
+    audio.volume = 0.5;
+
+    audio
+      .play()
+      .then(() => {
+        this.isPlaying = true;
+      })
+      .catch(() => {
+        this.isPlaying = false; // autoplay blocked
+      });
+  }
+
+  toggleMusic() {
+    const audio = document.getElementById("weddingSong") as HTMLAudioElement;
+    if (audio.paused) {
+      audio.play();
+      this.isPlaying = true;
+    } else {
+      audio.pause();
+      this.isPlaying = false;
+    }
   }
 
   setupCountdown() {
@@ -35,7 +59,6 @@ export class InvitationComponent {
 
     // Update the count down every 1 second
     var x = setInterval(() => {
-
       // Get today's date and time
       var now = new Date().getTime();
 
@@ -44,12 +67,14 @@ export class InvitationComponent {
 
       // Time calculations for days, hours, minutes and seconds
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      this.countdown = days + "d " + hours + "h "
-      + minutes + "m " + seconds + "s ";
+      this.countdown =
+        days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
       // If the count down is finished, write some text
       if (distance < 0) {
@@ -60,13 +85,11 @@ export class InvitationComponent {
   }
 
   copyBankAccount() {
-    const accNo = '123456789';
-    
+    const accNo = "164799156762";
+
     // Copy the text inside the text field
     navigator.clipboard.writeText(accNo);
-  
-    // Alert the copied text
-    //alert("Copied the text: " + accNo);
+
     this.showCopySuccess = true;
 
     // Hide message after 5 seconds
@@ -76,35 +99,36 @@ export class InvitationComponent {
   }
 
   submitRSVP() {
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbxVyOXCsZ6iogvkJDMyXdM9FTf__hQobLicdwnEx0eifB7lpKazUAr8vAvMIaXwufQ/exec';
+    const scriptURL =
+      "https://script.google.com/macros/s/AKfycbxVyOXCsZ6iogvkJDMyXdM9FTf__hQobLicdwnEx0eifB7lpKazUAr8vAvMIaXwufQ/exec";
     const formData = new FormData();
-    formData.append('Name', this.rsvp.name);
-    formData.append('Attendance', this.rsvp.attendance);
-    formData.append('Message', this.rsvp.message);
-  
+    formData.append("Name", this.rsvp.name);
+    formData.append("Attendance", this.rsvp.attendance);
+    formData.append("Message", this.rsvp.message);
+    formData.append("Pax", this.rsvp.pax);
+
     this.isSubmitting = true;
     this.showSuccess = false;
-  
-    fetch(scriptURL, { method: 'POST', body: formData })
-      .then(response => {
+
+    fetch(scriptURL, { method: "POST", body: formData })
+      .then((response) => {
         if (response.ok) {
-          console.log('Success!', response);
+          console.log("Success!", response);
           this.lastSubmittedName = this.rsvp.name;
           this.showSuccess = true;
-          this.rsvp = { name: '', attendance: '', message: '' };
-          setTimeout(() => this.showSuccess = false, 5000);
+          this.rsvp = { name: "", attendance: "", message: "", pax: "" };
+          setTimeout(() => (this.showSuccess = false), 5000);
         } else {
-          console.error('Google script returned error:', response);
-          alert('Something went wrong. Please try again.');
+          console.error("Google script returned error:", response);
+          alert("Something went wrong. Please try again.");
         }
       })
-      .catch(error => {
-        console.error('Error!', error.message);
-        alert('Something went wrong. Please try again.');
+      .catch((error) => {
+        console.error("Error!", error.message);
+        alert("Something went wrong. Please try again.");
       })
       .finally(() => {
         this.isSubmitting = false;
       });
   }
-  
 }
